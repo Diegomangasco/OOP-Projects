53187c4282291e5cc818bbfd75e7b2dd9a3a4d63
Diegomangasco <62801631+Diegomangasco@users.noreply.github.com>
2020-06-22 12:18:05 +0200

    Add files via upload

---
+++ Esame_Anomalie_2016/src/ticketing/Ticket.java
@@ -0,0 +1,74 @@
+package ticketing;
+
+/**
+ * Class representing the ticket linked to an issue or malfunction.
+ * 
+ * The ticket is characterized by a severity and a state.
+ */
+public class Ticket {
+    
+    /**
+     * Enumeration of possible severity levels for the tickets.
+     * 
+     * Note: the natural order corresponds to the order of declaration
+     */
+    public enum Severity { Blocking, Critical, Major, Minor, Cosmetic };
+    
+    /**
+     * Enumeration of the possible valid states for a ticket
+     */
+    public static enum State { Open, Assigned, Closed }
+    private int code;
+    private String userName;
+    private String componentPath;
+    private String description;
+    private Ticket.Severity severity;
+    private String solution;
+    private State state = State.Open;
+    public Ticket(int code, String userName, String componentPath, String description, Ticket.Severity severity) {
+    	this.code = code;
+    	this.description = description;
+    	this.userName = userName;
+    	this.componentPath = componentPath;
+    	this.severity = severity;
+    }
+    public int getId(){
+        return this.code;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+    
+    public Severity getSeverity() {
+        return this.severity;
+    }
+
+    public String getAuthor(){
+        return this.userName;
+    }
+    
+    public String getComponent(){
+        return this.componentPath;
+    }
+    
+    
+    public State getState(){
+        return this.state;
+    }
+    public void setStateA() {
+    	this.state = State.Assigned;
+    }
+    public void setStateC() {
+    	this.state = State.Closed;
+    }
+    public void setSolution(String solution) {
+    	this.solution = solution;
+    }
+    
+    public String getSolutionDescription() throws TicketException {
+        if(this.state != State.Closed)
+        	throw new TicketException();
+        return this.solution;
+    }
+}
