2e4c0da27f9cb72321a6a0360862ecb420e1c8ad
Diegomangasco <62801631+Diegomangasco@users.noreply.github.com>
2020-06-22 14:18:39 +0200

    Add files via upload

---
+++ Esame_Anomalie_2016/src/ticketing/Componente.java
@@ -0,0 +1,31 @@
+package ticketing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Componente {
+	private String name;
+	private Map<String, Componente> subComponents = new HashMap<>();
+	public Componente(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void addSub(String name) {
+		this.subComponents.put(name, new Componente(name));
+	}
+	public Map<String, Componente> getSub(){
+		return this.subComponents;
+	}
+	public boolean hasSub(String C) {
+		if(this.subComponents.containsKey(C))
+			return true;
+		else return false;
+	}
+	public Componente getSub(String S) {
+		return this.subComponents.get(S);
+	}
+}
